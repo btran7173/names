f918f59760efd5a540607129d1aa1e657f3ed268
btran7173 <btran7173@001LAB153010807.neisd.ad>
2015-10-20 12:39:49 -0500

    xfh

---
+++ hello/src/hello/hi.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int year = 2001;
+ int day = 24;
+ int rem;
+ rem = year % day;
+ System.out.print("My name is Brian Tran. My birthday is June, ");
+ System.out.print(day);
+   System.out.print(" ");
+     System.out.print(year);
+     System.out.print(". The remainder of is");
+     System.out.print(" ");
+     System.out.print(rem);
+     System.out.print(".");
+     System.out.println("hi");
+	}
+
+}
